49d29b7ca5be1060f99b9b0fa40b2998aeeaf6f0
Bloody Altair <22257078+BloodyAltair@users.noreply.github.com>
2018-08-24 03:24:37 +0300

    Update getUnsplashBg.php

--- getUnsplashBg.php
+++ getUnsplashBg.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Copyright © 2018 Denis Vadimov aka BloodyAltair
  * This file is part of UnsplashBackgroundUpdater.
  *
@@ -25,24 +25,26 @@
  */
 $config = json_decode (file_get_contents (dirname (__FILE__) . "/config.json"), true);
 header ("Content-Type: application/json");
-if (empty($_SERVER['HTTP_ORIGIN'])) die(json_encode (["success" => false, "error" => "Your request has no CORS headers"]));
-header ("Access-Control-Allow-Headers: *");
-header ("Access-Control-Request-Method: GET");
-if (preg_match ('/^https:\\/\\/(.*)(\.?)('.$config['allowed_domain'].')$/', $_SERVER['HTTP_ORIGIN'])) {
-	header ("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-} else {
-	die(json_encode (["success" => false, "error" => "Request is not allowed. Reason: CORS Policy"]));
+if (!empty($_SERVER['HTTP_ORIGIN'])) {
+	header ("Access-Control-Allow-Headers: *");
+	header ("Access-Control-Request-Method: GET");
+	if (preg_match ('/^https:\\/\\/(.*)(\.?)(' . $config['allowed_domain'] . ')$/', $_SERVER['HTTP_ORIGIN'])) {
+		header ("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+	} else {
+		die(json_encode (["success" => false, "error" => "Request is not allowed. Reason: CORS Policy"]));
+	}
 }
 
 if (!empty($config['accessToken'])) {
 	$url = "https://api.unsplash.com/photos/random?per_page=1&count=1&w=" . $config['width'] . "&h=" . $config['height'] . "&orientation=" . $config['orientation'] . ( ( !empty($config['query']) ) ? "&query=" . $config['query'] : "" ) . "&client_id=" . $config['accessToken'];
 	$ch = curl_init ();
-	curl_setopt ($ch, CURLOPT_HEADER, 0);
-	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt ($ch, CURLOPT_HEADER, false);
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt ($ch, CURLOPT_URL, $url);
 	if (!empty($headers)) curl_setopt ($ch, CURLOPT_HTTPHEADER, $headers);
 	$json = json_decode (curl_exec ($ch), true)[0];
 	curl_close ($ch);
+
 	$image_url = $json['urls']['custom'];
 	$image_user_name = $json['user']['name'];
 	$image_user_url = $json['user']['links']['html'];
